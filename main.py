6672ce4ca65587a2d3ec3a69e362c111513efd82
co0lestnerd <overlooked723@gmail.com>
2025-07-01 19:54:52 +1000

    CH3:Report L1

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 def main():
 
+    print(f"============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    
     #call function to take relative book path and turn it to a string
     output = get_book_text("books/frankenstein.txt")
 
-    #call function to accept string and count the words 
+    #call function from stats to accept string and count the words 
+    print(f"----------- Word Count ----------")
     count = get_num_words(output)
-    print(f"{count} words found in the document")
+    print(f"Found {count} total words")
 
-    #call function to accept string and count the char 
+    #call function from stats to accept string and count the char
+    print(f"--------- Character Count -------")
     char_count = get_num_char(output)
-    print(char_count)
+
+    #call function from stats to accept dictionary and sort by numbers descending
+    sorted_chars = sort_on(char_count)
+
+    #for each item in list, check if the key is an alphabetic character, and then if so, print it to the console
+    for item in sorted_chars:
+        storedChar = item["char"]
+        storedNum = item["num"]
+
+        if storedChar.isalpha():
+            print(f"{storedChar}: {storedNum}")
+
+    print(f"============= END ===============")
 
 #function that takes filepath and converts it to a string
 def get_book_text(path_to_file):
@@ -21,5 +38,6 @@ def get_book_text(path_to_file):
 
 from stats import get_num_words
 from stats import get_num_char
+from stats import sort_on
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,30 @@ def get_num_char(text):
             characters[char] = 1
 
     return characters
+
+def sort_on(chardict):
+    #create a new list
+    charValues = []
+
+    #for each letter in the supplied dictionary(chardict) add it's letter and letter value to a new dictionary in a list
+    for letter in chardict:
+        letter_value = chardict[letter]
+
+        formatdict = {
+            'char': letter,
+            'num': letter_value
+            }
+
+        charValues.append(formatdict)
+
+    #use the sort method to sort greatest to least and create a function to set the sorting criteria
+    def sortNums(tempdict):
+        return tempdict["num"]
+
+    charValues.sort(reverse=True, key=sortNums)
+
+    return charValues
+    
+    
+    
+
